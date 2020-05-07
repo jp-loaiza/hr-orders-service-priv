ef25ade63dc573011b1ccc3f00bbaf66c3b190ed
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2020-05-07 15:52:33 -0400

    HRC-1855 Define order type

--- orders.d.ts
+++ orders.d.ts
@@ -7,3 +7,61 @@ interface Env {
   SFTP_PRIVATE_KEY: string,
   SFTP_INCOMING_ORDERS_PATH: string
 }
+
+type Price = {
+  type: string,
+  currencyCode: string,
+  centAmount: number,
+  fractionDigits: number
+}
+
+type TaxPortion = {
+  rate: number,
+  amount: Price,
+  name: string
+}
+
+type LineItem = {
+  variant: {
+    sku: string,
+    prices: Array<{ value: Price }>
+  },
+  quantity: number,
+  custom: {
+    fields: {
+      isGift: boolean
+    }
+  }
+}
+
+type Address = {
+  additionalStreetInfo: string,
+  postalCode: string,
+  city: string,
+  state: string,
+  country: string
+}
+
+type Order = {
+  type: string,
+  id: string,
+  orderNumber: string,
+  createdAt: string,
+  customerId?: string,
+  anonymousId?: string,
+  customerEmail: string,
+  totalPrice: Price,
+  taxedPrice: {
+    totalNet: Price,
+    totalGross: Price,
+    taxPortions: Array<TaxPortion>
+  },
+  lineItems: Array<LineItem>,
+  shippingAddress: Address,
+  billingAddress: Address
+}
+
+export {
+  Env,
+  Order
+}
