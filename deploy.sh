6237c719497a4ece702ca824e3e4bca5257c1318
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2021-02-24 14:49:14 -0500

    HRC-4185 Add new environment variables to deploy script, etc.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ kubectl create configmap hr-orders-service \
   --from-literal=SFTP_HOST=$SFTP_HOST_KUBERNETES \
   --from-literal=SFTP_PORT=$SFTP_PORT \
   --from-literal=SFTP_INCOMING_ORDERS_PATH=$SFTP_INCOMING_ORDERS_PATH \
+  --from-literal=CJ_CID=$CJ_CID \
+  --from-literal=CJ_CONVERSION_BASE_URL=$CJ_CONVERSION_BASE_URL \
+  --from-literal=CJ_TYPE=$CJ_TYPE \
   --from-literal=CT_PROJECT_KEY=$CT_PROJECT_KEY \
   --from-literal=CT_OAUTH_HOST=$CT_OAUTH_HOST \
   --from-literal=CT_HOST=$CT_HOST \
@@ -15,9 +18,11 @@ kubectl create configmap hr-orders-service \
   --from-literal=EMAIL_API_URL=$EMAIL_API_URL_KUBERNETES \
   --from-literal=JESTA_API_HOST=$JESTA_API_HOST \
   --from-literal=SEND_ALGOLIA_INFO_INTERVAL=$SEND_ALGOLIA_INFO_INTERVAL \
+  --from-literal=SEND_CJ_CONVERSIONS_INTERVAL=$SEND_CJ_CONVERSIONS_INTERVAL \
   --from-literal=SHOULD_CHECK_FOR_STUCK_ORDERS=$SHOULD_CHECK_FOR_STUCK_ORDERS \
   --from-literal=SHOULD_SEND_ORDER_UPDATES=$SHOULD_SEND_ORDER_UPDATES \
   --from-literal=SHOULD_SEND_ALGOLIA_INFO=$SHOULD_SEND_ALGOLIA_INFO \
+  --from-literal=SHOULD_SEND_CJ_CONVERSIONS=$SHOULD_SEND_CJ_CONVERSIONS \
   --from-literal=SHOULD_UPLOAD_ORDERS=$SHOULD_UPLOAD_ORDERS \
   --from-literal=SHOULD_SEND_NOTIFICATIONS=$SHOULD_SEND_NOTIFICATIONS \
   --from-literal=STALE_ORDER_CUTOFF_TIME_MS=$STALE_ORDER_CUTOFF_TIME_MS \
@@ -33,6 +38,7 @@ kubectl create secret generic hr-orders-service \
   --from-literal=ALGOLIA_API_KEY=$ALGOLIA_API_KEY \
   --from-literal=SFTP_USERNAME=$SFTP_USERNAME \
   --from-literal=SFTP_PRIVATE_KEY=$SFTP_PRIVATE_KEY \
+  --from-literal=CJ_SIGNATURE=$CJ_SIGNATURE \
   --from-literal=CT_CLIENT_ID=$CT_CLIENT_ID \
   --from-literal=CT_CLIENT_SECRET=$CT_CLIENT_SECRET \
   --from-literal=EMAIL_API_USERNAME=$EMAIL_API_USERNAME \
