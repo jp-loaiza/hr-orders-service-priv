3873ba881698c536a3fd2a7eaa73c0f33b55abd3
Tamas Marki <tamas.m@myplanet.com>
2021-07-12 10:49:31 -0600

    HRC-4777 First steps with Narvar job, trying to use existing patterns

---
+++ narvar.js
@@ -0,0 +1,55 @@
+import fetch from 'node-fetch'
+import base64 from 'base-64'
+
+const baseUrl = process.env.NARVAR_BASE_URL
+const username = process.env.NARVAR_USERNAME
+const password = process.env.NARVAR_PASSWORD
+
+
+/**
+ * @param {string} path Path to the Api
+ * @param {*} options Options with the request body and authentication parameters
+ * @returns Result of request if it was successful
+ * @throws Error if response is not successful
+ * */
+
+const makeNarvarRequest = async (path, options) => {
+  console.log('narvar request:')
+  console.log(path)
+  console.log(baseUrl)
+  console.log(options)
+  const response = await fetch(baseUrl + path, options)
+  const result = await response.json()
+  console.log('result:')
+  console.log(result)
+  if (response.ok) {
+    if (result.status === 'FAILURE') {
+      throw new Error(JSON.stringify(result))
+    }
+    return result
+  }
+  throw new Error(JSON.stringify(result))
+}
+
+/**
+ * 
+ * @param {import('./orders').Order} order The order to send to Narvar
+ * @returns Result of the request
+ */
+
+const sendOrderToNarvar = async (order) => {
+  // convert order to Narvar format
+  const options = {
+    body: JSON.stringify(order),
+    headers: {
+      Authorization: `Basic ${base64.encode(username + ':' + password)}`,
+      'Content-Type': 'application/json'
+    },
+    method: 'POST'
+  }
+  return makeNarvarRequest('/orders', options)
+}
+  
+export default {
+  sendOrderToNarvar
+}
