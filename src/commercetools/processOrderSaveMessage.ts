0805225517b0c2cafe4b879347ad4fcb813fe209
allan Ruka <allan.ruka@orium.com>
2023-03-08 16:44:53 -0600

    fix: HRC-5778: format to view log errors (#483)

--- src/commercetools/processOrderSaveMessage.ts
+++ src/commercetools/processOrderSaveMessage.ts
@@ -1,20 +1,24 @@
 import { OrderSaveMessage } from "../events/OrderSaveMessage";
 import { apiRoot } from "../commercetools/ctClientV2";
-import logger from '../logger'
+import logger, { serializeError } from '../logger'
 
 export const processOrderSaveMessage = async (msg: OrderSaveMessage) => {
   try {
-    const { body: { version } } = await apiRoot.orders().withOrderNumber({orderNumber: msg.ORDER_NUMBER}).get().execute()
+    const { body: { version } } = await apiRoot.orders().withOrderNumber({ orderNumber: msg.ORDER_NUMBER }).get().execute()
     logger.info(`ProcessOrderSave start, Order number: ${msg.ORDER_NUMBER}, version: ${version}, actions: ${JSON.stringify(msg.ACTIONS)}`)
-    await apiRoot.orders().withOrderNumber({orderNumber: msg.ORDER_NUMBER}).post({
+    await apiRoot.orders().withOrderNumber({ orderNumber: msg.ORDER_NUMBER }).post({
       body: {
         actions: msg.ACTIONS,
         version,
       },
     })
-    .execute()
+      .execute()
     logger.info(`ProcessOrderSave succeed, Order number: ${msg.ORDER_NUMBER}`)
-  } catch (error: any) {
-    logger.error(`ProcessOrderSave failed, Order number: ${msg.ORDER_NUMBER}`)
+  } catch (error) {
+    logger.error({
+      type: 'process_order_save',
+      message: `ProcessOrderSave failed, Order number: ${msg.ORDER_NUMBER}`,
+      error: serializeError(error)
+    })
   }
 }
